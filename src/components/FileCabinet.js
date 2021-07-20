53393c68721f73dffca9cd573f750d2ccd91927d
jeanmarcduong <jeanmarcduong98@gmail.com>
2021-07-20 00:51:44 -0400

    File Cabinet added

--- src/components/FileCabinet.js
+++ src/components/FileCabinet.js
@@ -5,16 +5,23 @@ const FileCabinet = () => {
   return (
     <React.Fragment>
       <div className={FileCSS.container}>
-        <h1>Profile</h1>
+        <h1>Medical Information</h1>
 
         <h4 className={FileCSS}>Name</h4>
         <p className={FileCSS.profileInfo}>John Doe</p>
 
-        <h4 className={FileCSS}>Wallet</h4>
-        <p className={FileCSS.profileInfo}>$12345</p>
+        <h4 className={FileCSS}>Medication</h4>
+        <p className={FileCSS.profileInfo}>None</p>
 
-        <h4 className={FileCSS}>Email</h4>
-        <p className={FileCSS.profileInfo}>johndoe@email.com</p>
+        <h4 className={FileCSS}>Diagnoses</h4>
+        <p className={FileCSS.profileInfo}>None</p>
+        <h4 className={FileCSS}>Test Results</h4>
+        <p className={FileCSS.profileInfo}>None </p>
+        <h4 className={FileCSS}>Allergies</h4>
+        <p className={FileCSS.profileInfo}>None</p>
+        <h4 className={FileCSS}>Password</h4>
+        <p className={FileCSS.profileInfo}>sZKOn1xEZ4T9oDzp </p>
+        <button className="submit">Decrypt</button>
       </div>
     </React.Fragment>
   );
